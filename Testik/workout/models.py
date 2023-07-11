cc7f486440ff046d6425ea6b32e6db0e017c3549
To-nevim <114871514+To-nevim@users.noreply.github.com>
2023-07-11 23:59:19 +0200

    I think it might work now

--- Testik/workout/models.py
+++ Testik/workout/models.py
@@ -4,3 +4,6 @@ class Workout(models.Model):
     part = models.CharField(max_length=20)
     exercises = models.IntegerField()
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.part} for {self.duration}"
